126631e46c5d2cc08d1022cdc2e0dac2397c83a2
lhhmmiii <lehuuhung30023010@gmail.com>
2025-05-10 16:09:58 +0700

    update main.py

--- backend/writing/main.py
+++ backend/writing/main.py
@@ -1,16 +1,33 @@
 import os
 import sys
-
+from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from writing.routers import grammar_check_router, level_analysis_router, writing_feedback_router,\
                             user_router, auth_router, chat_memory_router, vocabulary_support_router,\
-                            agent_router
+                            agent_router, extract_office_file_router, extract_html_file_router,\
+                            extract_table_file_router, document_qa_router
 
+from writing.dependencies import initialize_storage_context
+from writing.config.llm import gemini
+from writing.config.setting import embed_model, setting
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.llm = gemini
+    app.state.embed_model = embed_model()
+    setting(llm = app.state.llm, embed_model = app.state.embed_model)
+    app.state.storage_context = initialize_storage_context("StudyEnglishData", "Grammar")
+    yield
+    # Clean up resources
+    app.state.storage_context = None
+    app.state.llm = None 
+    app.state.embed_model = None
+    
+
+app = FastAPI(lifespan=lifespan)
 
 # Allow CORS for frontend running on a different por
 app.add_middleware(
@@ -31,6 +48,10 @@ app.include_router(auth_router)
 app.include_router(chat_memory_router)
 app.include_router(vocabulary_support_router)
 app.include_router(agent_router)
+app.include_router(extract_office_file_router)
+app.include_router(extract_html_file_router)
+app.include_router(extract_table_file_router)
+app.include_router(document_qa_router)
 
 @app.get("/")
 async def root():
