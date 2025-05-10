b955b0831c80a780b46ebe7206eb5ad1e915acd7
lhhmmiii <lehuuhung30023010@gmail.com>
2025-05-10 16:09:35 +0700

    add create index search

--- backend/writing/database/__init__.py
+++ backend/writing/database/__init__.py
@@ -1,7 +1,8 @@
-from .mongodb import connect_to_mongo
+from .mongodb import connect_to_mongo, create_vector_search_index
 from .redis_client import redis_client
 
 __all__ = [
     "connect_to_mongo",
-    "redis_client"
+    "redis_client",
+    "create_vector_search_index"
 ]
--- backend/writing/database/mongodb.py
+++ backend/writing/database/mongodb.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from pymongo.operations import SearchIndexModel
 from dotenv import load_dotenv
 import os
 
@@ -12,3 +13,29 @@ def connect_to_mongo(db_name: str, collection_name):
     db = client[db_name]
     collection = db[collection_name]
     return collection
+
+def create_vector_search_index(mongo_uri: str = mongo_uri, db_name: str = 'StudyEnglishData', collection_name: str = 'Grammar'):
+    mongo_client = MongoClient(mongo_uri)
+    collection = mongo_client[db_name][collection_name]
+
+    search_index_model = SearchIndexModel(
+        definition={
+            "fields": [
+                {
+                    "type": "vector",
+                    "path": "embedding",
+                    "numDimensions": 1024,
+                    "similarity": "cosine"
+                },
+                {
+                    "type": "filter",
+                    "path": "metadata.user_id"
+                }
+            ]
+        },
+        name="vector_index",
+        type="vectorSearch"
+    )
+
+    collection.create_search_index(model=search_index_model)
+    print("✅ Search index 'vector_index' created successfully.")
