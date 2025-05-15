3f426abb123f5dde547c3940c3697ffd30218964
lhhmmiii <lehuuhung30023010@gmail.com>
2025-05-15 15:15:47 +0700

    set redis host default is localhost, when run docker is redis-server

--- backend/writing/database/redis_client.py
+++ backend/writing/database/redis_client.py
@@ -1,9 +1,12 @@
 from redis import Redis
+import os
 
+redis_host = os.getenv("REDIS_HOST", "localhost")
 
 redis_client = Redis(
-    host="localhost",
+    host=redis_host,
     port=6379,
     db=0,
     decode_responses=True  # Trả string thay vì bytes
 )
+
