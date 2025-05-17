066714f3585a61c755c1ece36201c070c304a7ff
lhhmmiii <lehuuhung30023010@gmail.com>
2025-05-17 22:36:23 +0700

    update file

--- backend/app/config/__init__.py
+++ backend/app/config/__init__.py
@@ -1,8 +1,9 @@
 from .prompts import grammar_check_prompt, level_analysis_prompt, writing_feedback_prompt, writing_tool_selector_prompt,\
                     generate_vocabulary_prompt, english_vietnamese_dictionary_prompt, grammar_explanation_prompt,\
-                    sentence_parsing_prompt, example_generator_prompt, conversation_simulator_prompt,\
+                    sentence_parsing_prompt, example_generator_prompt, roleplay_prompt,\
                     error_correction_prompt, faq_knowledge_base_prompt, quick_tip_prompt,\
-                    fall_to_gemini_prompt, feedback_prompt
+                    fall_to_gemini_prompt, feedback_prompt, conversation_feedback_prompt,\
+                    conversation_agent_prompt, roleplay_param_extraction_prompt
 from .llm import gemini
 from .setting import embed_model, setting
 
@@ -14,16 +15,21 @@ __all__ = [
     "grammar_check_prompt",
     "level_analysis_prompt",
     "writing_feedback_prompt",
+    # Writing Agent
     "writing_tool_selector_prompt",
     "generate_vocabulary_prompt",
     "english_vietnamese_dictionary_prompt",
     "grammar_explanation_prompt",
     "sentence_parsing_prompt",
     "example_generator_prompt",
-    "conversation_simulator_prompt",
     "error_correction_prompt",
     "feedback_prompt",
     "faq_knowledge_base_prompt",
     "quick_tip_prompt",
     "fall_to_gemini_prompt",
+    # Conversation Agent
+    "roleplay_prompt",
+    "conversation_feedback_prompt",
+    "conversation_agent_prompt",
+    "roleplay_param_extraction_prompt",
 ]
