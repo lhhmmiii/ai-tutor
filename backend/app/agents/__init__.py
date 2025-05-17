d8fda4d1acbcf6a414a0e4d3bdaabe5649bf53f4
lhhmmiii <lehuuhung30023010@gmail.com>
2025-05-17 22:37:19 +0700

    conversation agents

--- backend/app/agents/__init__.py
+++ backend/app/agents/__init__.py
@@ -1,7 +1,9 @@
 from .writing_agent import WritingAgentHandler
+from .conversation_agent import ConversationAgentHandler
 from .base import BaseAgentHandler
 
 __all__ = [
     "BaseAgentHandler",
-    "WritingAgentHandler"
+    "WritingAgentHandler",
+    "ConversationAgentHandler",
 ]
---
+++ backend/app/agents/conversation_agent.py
@@ -0,0 +1,33 @@
+import os
+import sys
+# Thêm đường dẫn cha vào sys.path để import module nội bộ
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+from app.tools import ConversationTools
+from app.config.prompts import conversation_agent_prompt
+from app.config.llm import gemini
+from app.agents.base import BaseAgentHandler
+
+
+class ConversationAgentHandler(BaseAgentHandler):
+    def __init__(self, verbose: bool = True):
+        tools = ConversationTools(llm=gemini).get_tools()
+        super().__init__(
+            agent_name="ConversationAgent",
+            llm=gemini,
+            tools=tools,
+            system_prompt=conversation_agent_prompt,
+            verbose=verbose,
+        )
+
+
+import asyncio
+
+user_input = "I want to practice my conversation skills with a native speaker\
+            with topic: 'Ordering food at a restaurant', role: 'waiter', context: 'a restaurant'"
+async def conversation_agent(user_id: str = "", user_input: str = "") -> str:
+    agent = ConversationAgentHandler()
+    return await agent.run(user_input=user_input, user_id=user_id)
+
+if __name__ == "__main__":
+    result = asyncio.run(conversation_agent(user_id="123", user_input=user_input))
+    print(result)
